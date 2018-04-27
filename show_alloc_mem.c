f9a0a14968bfcf8f0b63b21c1385e822522ff1ac
Jules BARBIER <jubarbie@e3r2p1.42.fr>
2018-04-27 17:55:28 +0200

    better show output

--- show_alloc_mem.c
+++ show_alloc_mem.c
@@ -6,54 +6,67 @@
 /*   By: jubarbie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/25 11:34:51 by jubarbie          #+#    #+#             */
-/*   Updated: 2018/04/26 18:50:57 by jubarbie         ###   ########.fr       */
+/*   Updated: 2018/04/27 17:53:23 by jubarbie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
-static void		print_block(void *p)
+static size_t	print_block(void *p)
 {
-	if (p == NULL)
-		ft_putendl("Attempt to print NULL block");
-	else
-		printf("[%zu|%d] %zu bytes [%zu] %p\n", hdb_size(p), hdb_alloc(p),
-				payload_size(p), ftb_size(p), p);
+	size_t	s;
+
+	s = 0;
+	if (hdb_alloc(p) == 1)
+	{
+		s += hdb_size(p);
+		printf("%p - %p : %zu octetcs\n", hdb(p), (char *)hdb(p) + hdb_size(p),
+				payload_size(p));
+	}
+	return (s);
 }
 
-static void		print_room(void *room, int i, char *name)
+static size_t	print_room(void *room)
 {
 	void	*block;
+	size_t	s;
 
-	printf("%d ", i);
-	printf("%s : %p - %p : %lu bytes\n", name, room, room_limit(room), room_size(room));
+	s = 0;
 	block = room;
 	while ((char *)block < (char *)room_limit(room))
 	{
-		print_block(block);
+		s += print_block(block);
 		block = next_block(block);
 	}
+	return (s);
 }
 
-static void		print_mem(void *ptr, char *name)
+static size_t	print_mem(void *ptr, char *name)
 {
 	void	*room;
-	int		i;
-	
-	i = 0;
+	size_t	s;
+
+	s = 0;
 	room = ptr;
-	while(room != NULL)
-	{	
-		print_room(room, i, name);		
-		room = next_room(room);
-		i++;
+	if (ptr != NULL)
+	{
+		printf("%s : %p\n", name, get_hd_room(ptr));
+		while(room)
+		{	
+			s += print_room(room);		
+			room = next_room(room);
+		}
 	}
+	return (s);
 }
 
 void			show_alloc_mem(void)
 {
-	ft_putendl("----- Memory viewer -----");
-	print_mem(g_mem_tiny, "TINY");
-	print_mem(g_mem_small, "SMALL");
-	print_mem(g_mem_medium, "MEDIUM");
+	size_t	s;
+
+	s = 0;
+	s += print_mem(g_mem_tiny, "TINY");
+	s += print_mem(g_mem_small, "SMALL");
+	s += print_mem(g_mem_medium, "LARGE");
+	printf("Total : %zu octets\n", s);
 }
